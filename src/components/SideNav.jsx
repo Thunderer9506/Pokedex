c9d5d1093dd41909bdcc98ab1dcf1106b0ca2b90
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-06-09 18:05:33 +0530

    Refactor SideNav component to use default export and implement Pokémon navigation

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,20 @@
-export function SideNav() {
+import {first151Pokemon,getFullPokedexNumber} from "../utils"
+
+export default function SideNav() {
     return (
-        <nav></nav>
+        <nav>
+            <div className={"header"}>
+                <h1 className="text-gradient">Pokédex</h1>
+            </div>
+            <input type="text" />
+            {first151Pokemon.map((pokemon, pokemonIndex) => {
+                return (
+                    <button key={pokemonIndex} className={'nav-card'}>
+                        <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                        <p>{pokemon}</p>
+                    </button>
+                )
+            })}
+        </nav>
     )
 }
