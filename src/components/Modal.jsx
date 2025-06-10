7bfc44fa42809441cbb322212c3cdb3ae2939fca
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-06-10 19:46:52 +0530

    Refactor Modal component to use React Portal for rendering and handle modal close functionality

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,15 @@
-export default function Model() {
-    return
+import ReactDom from "react-dom"
+
+export default function Modal(props) {
+    const {children, handelCloseModal} = props
+    return ReactDom.createPortal(
+        <div className="modal-container">
+            <button onClick={handelCloseModal}
+            className="modal-underlay"></button>
+            <div className="modal-content">
+                {children}
+            </div>
+        </div>,
+        document.getElementById('portal')
+    )
 }
