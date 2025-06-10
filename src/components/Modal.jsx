19c621a159e7b41c7b576d97fe54b9c6b9c26e0f
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-06-10 20:54:06 +0530

    Refactor Modal component: simplify button rendering in modal underlay

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,8 +4,7 @@ export default function Modal(props) {
     const {children, handelCloseModal} = props
     return ReactDom.createPortal(
         <div className="modal-container">
-            <button onClick={handelCloseModal}
-            className="modal-underlay"></button>
+            <button onClick={handelCloseModal} className="modal-underlay" />
             <div className="modal-content">
                 {children}
             </div>
