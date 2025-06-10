3068b6918645c04ff84e95126222b77561f988f8
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-06-10 20:53:39 +0530

    Refactor SideNav component to accept props and implement search functionality for filtering Pokémon

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react"
 import {first151Pokemon,getFullPokedexNumber} from "../utils"
 
-export default function SideNav() {
+export default function SideNav(props) {
+    const { selectedPokemon, setSelectedPokemon} = props
+    const [ searchValue, setSearchValue ] = useState('')
+    const filteredPokemon = first151Pokemon.filter((ele,eleIndex) => {
+        if ((getFullPokedexNumber(eleIndex)).includes(searchValue))
+        {return true}
+        if (ele.toLowerCase().includes(searchValue.toLowerCase())) {return true}
+        return false
+    })
     return (
         <nav>
             <div className={"header"}>
                 <h1 className="text-gradient">Pokédex</h1>
             </div>
-            <input type="text" />
-            {first151Pokemon.map((pokemon, pokemonIndex) => {
+            <input placeholder="E.g. 001 or Bulba..." type="text" onChange={(e) => {
+                setSearchValue(e.target.value)
+            }}/>
+            {filteredPokemon.map((pokemon, pokemonIndex) => {
+                const truePokedexNumber = first151Pokemon.indexOf(pokemon)
                 return (
-                    <button key={pokemonIndex} className={'nav-card'}>
-                        <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                    <button onClick={() => {
+                        setSelectedPokemon(truePokedexNumber)
+                    }} key={pokemonIndex} className={'nav-card'+
+                        (pokemonIndex === selectedPokemon ? ' nav-card-selected': ' ')
+                    }>
+                        <p>{getFullPokedexNumber(truePokedexNumber)}</p>
                         <p>{pokemon}</p>
                     </button>
                 )
